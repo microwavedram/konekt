00cf33576a37c9f036438059d18a80278351a5a3
microwavedram <89029844+microwavedram@users.noreply.github.com>
2022-04-14 01:09:07 +0100

    Create f3.lua

---
+++ modules/f3.lua
@@ -0,0 +1,19 @@
+-- really useful for getting boundarys
+
+local config = require("config")
+local ui = require("ui")
+
+local monitor = ui.wrap(peripheral.wrap("right"))
+
+local function render(x,y)
+    monitor:blite(1,1,"DEBUG","0","e")
+    monitor:blite(1,2,string.format("X:%s Y:%s",x,y),"0","e")
+end
+
+while true do
+    local event, side, x, y
+    repeat
+        event, side, x, y = os.pullEvent("monitor_touch")
+    until side == config.front_monitor_id
+    render(x,y)
+end
